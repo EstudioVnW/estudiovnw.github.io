b39c4ddae553d86762c5233de46175342b6fda43
Anna Luiza Araujo <anna.luiza.araujo@hotmail.com>
2018-06-14 15:04:38 -0300

    ajustes mobile

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-(function() {
+// (function() {
 
-  smoothScroll.init({
-    selector: '[data-scroll]', // Selector for links (must be a class,ID,data attribute, or element tag)
-    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
-    speed: 500, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-    callback: function ( anchor, toggle ) {} // Function to run after scrolling
-  });
+//   smoothScroll.init({
+//     selector: '[data-scroll]', // Selector for links (must be a class,ID,data attribute, or element tag)
+//     selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+//     speed: 500, // Integer. How fast to complete the scroll in milliseconds
+//     easing: 'easeInOutCubic', // Easing pattern to use
+//     offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+//     callback: function ( anchor, toggle ) {} // Function to run after scrolling
+//   });
 
   var section = document.querySelectorAll('section');
   var sections = {};
@@ -16,9 +16,9 @@
   var menuItem = document.querySelectorAll('.header__menu-item');
   var menuList = document.querySelector('.header__menu');
 
-  Array.prototype.forEach.call(section, function(e) {
-    sections[e.id] = e.offsetTop;
-  });
+  // Array.prototype.forEach.call(section, function(e) {
+  //   sections[e.id] = e.offsetTop;
+  // });
 
   window.onscroll = function() {
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
