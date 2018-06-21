af359b09f403678547991fdb377d89b6e98ae1fc
Anna Luiza Araujo <anna.luiza.araujo@hotmail.com>
2018-06-21 16:37:22 -0300

    finanlização do slideShow no mobile

--- main.js
+++ main.js
@@ -48,38 +48,103 @@
   };
 })();
 
-var slidesIndex = 1;
-showDivs(slidesIndex);
+var slidesIncentivadores = 1;
+showIncentivadores(slidesIncentivadores);
 
-function plusDivs(n){
-  showDivs(slidesIndex += n);
+function plusIncentivadores(n){
+  showIncentivadores(slidesIncentivadores += n);
 }
 
-function showDivs(n){
+function showIncentivadores(n){
   var i;
   var x = document.getElementsByClassName("conteudo-incentivo-caixa");
-  if (n > x.length) {slidesIndex = 1}
-  if (n < 1 ) {slidesIndex = x.length};
+  if (n > x.length) {slidesIncentivadores = 1}
+  if (n < 1 ) {slidesIncentivadores = x.length};
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slidesIndex-1].style.display = "flex"
+  x[slidesIncentivadores-1].style.display = "flex"
 };
 
-var slideIndex = 1;
-showDiv(slideIndex);
 
-function plusSites(n){
-  showDiv(slideIndex += n);
-}
+var changeDesk;
+var elementDesk = document.querySelectorAll(".conteudo-projetos-caixa");
 
-function showDiv(n){
-  var i;
-  var x = document.getElementsByClassName("conteudo-projetos-caixa");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1 ) {slideIndex = x.length};
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+const codProjetosDesk = function(){
+  if (window.innerWidth > '768') {
+    var i;
+    for(i = 0;i < elementDesk.length;i++){
+      elementDesk[i].classList.add("sliderDesk")
+    }
+
+    var slideProjetosDesk = 1;
+    showProjetosDesk(slideProjetosDesk);
+
+    changeDesk = function plusProjetosDesk(n){
+      showProjetosDesk(slideProjetosDesk += n);
+    }
+
+    function showProjetosDesk(n){
+      var i;
+      var x = document.getElementsByClassName("sliderDesk");
+      if (n > x.length) {slideProjetosDesk = 1}
+      if (n < 1 ) {slideProjetosDesk = x.length};
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      x[slideProjetosDesk-1].style.display = "flex"
+    };
+  }else if (window.innerWidth <= '768') {
+    var i;
+    for(i = 0;i < elementDesk.length;i++){
+      elementDesk[i].classList.remove("sliderDesk");
+      elementDesk[i].style.display = "none"
+    }
   }
-  x[slideIndex-1].style.display = "flex"
+};
+
+
+var change;
+var element = document.querySelectorAll(".conteudo-projetos-caixa--mobile");
+
+const codProjetosMob = function(){
+  if (window.innerWidth <= '768') {
+    var i;
+    for(i = 0;i < element.length;i++){
+      element[i].classList.add("sliderMob")
+    }
+
+    var slideProjetos = 1;
+    showProjetos(slideProjetos);
+
+    change = function plusProjetos(n){
+      showProjetos(slideProjetos += n);
+    }
+
+    function showProjetos(n){
+      var i;
+      var x = document.getElementsByClassName("sliderMob");
+      if (n > x.length) {slideProjetos = 1}
+      if (n < 1 ) {slideProjetos = x.length};
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      x[slideProjetos-1].style.display = "flex"
+    };
+  }else if (window.innerWidth > '768') {
+    var i;
+    for(i = 0;i < element.length;i++){
+      element[i].classList.remove("sliderMob");
+      element[i].style.display = "none"
+    }
+  }
+};
+
+codProjetosDesk();
+
+codProjetosMob();
+
+function resize(){
+  codProjetosDesk();
+  codProjetosMob()
 };
