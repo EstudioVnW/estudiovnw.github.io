f074ce4954ff9beb6b55374c983a67a5ef672775
romulotunala <romulotunala@gmail.com>
2018-07-19 13:37:02 -0300

    Ajuste click menu mobile js

--- main.js
+++ main.js
@@ -96,7 +96,28 @@ codProjetosDesk();
 
 codProjetosMob();
 
+
+var menu = document.getElementById('menu')
+
+const hiddenMenu = function(){
+  if(window.innerWidth <= '768'){
+    menu.style.display = 'none'
+  }else if(window.innerWidth > '768'){
+    menu.style.display = 'flex'
+  }
+}
+ hiddenMenu();
+
+function showMenu(){
+  if( menu.style.display === 'none'){
+    menu.style.display = 'flex';
+  }else{
+    menu.style.display = 'none';
+  }
+};
+
 function resize(){
   codProjetosDesk();
   codProjetosMob()
+  hiddenMenu();
 };
